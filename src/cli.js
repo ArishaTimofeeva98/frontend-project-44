69d78d3c036533f93a42ed06bd83f4cea5fb2cf8
ArishaTimofeeva98 <arisha_timofeeva_1998@mail.ru>
2023-03-11 22:22:41 +0300

    +2

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 function getPlayerName() {
+  console.log('Hello');
   const playerName = readlineSync.question('What is your name: ');
   return playerName;
 }
